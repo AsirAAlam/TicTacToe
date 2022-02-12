5efbc386cf5c5f4542d383fc0a03583eb58c857c
AsirAAlam <46213673+AsirAAlam@users.noreply.github.com>
2022-02-11 21:58:57 -0500

    move html

--- js/GameView.js
+++ js/GameView.js
@@ -1,26 +1,6 @@
 export default class GameView {
   constructor(root) {
     this.root = root;
-    this.root.innerHTML = `
-      <div class="header">
-        <div class="header__turn"></div>
-        <div class="header__status"></div>
-        <button type="button" class="header__restart">
-          <i class="material-icons">refresh</i>
-        </button>
-      </div>
-      <div class="board">
-        <div class="board__tile" data-index="0"></div>
-        <div class="board__tile" data-index="1"></div>
-        <div class="board__tile" data-index="2"></div>
-        <div class="board__tile" data-index="3"></div>
-        <div class="board__tile" data-index="4"></div>
-        <div class="board__tile" data-index="5"></div>
-        <div class="board__tile" data-index="6"></div>
-        <div class="board__tile" data-index="7"></div>
-        <div class="board__tile" data-index="8"></div>
-      </div>
-    `;
 
     this.onTileClick = undefined;
     this.onRestartClick = undefined;
